7b448518906aab3a66a4407d9110cb8607d8d800
Sus Verwimp <sus.verwimp@hotmail.com>
2021-07-14 17:04:37 +0200

    TYPE remove duplicate tag

--- publish.sh
+++ publish.sh
@@ -4,5 +4,5 @@ set -eux
 VERSION=2.0.0
 TAG=$VERSION
 
-docker tag feder8/config-server:$TAG:$TAG $THERAPEUTIC_AREA_URL/$THERAPEUTIC_AREA/config-server:$TAG
+docker tag feder8/config-server:$TAG $THERAPEUTIC_AREA_URL/$THERAPEUTIC_AREA/config-server:$TAG
 docker push $THERAPEUTIC_AREA_URL/$THERAPEUTIC_AREA/config-server:$TAG
